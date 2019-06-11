3e742ac13b35c1d0d6f26a01d9c378621687c1ae
Sal Amato <enptfb55@gmail.com>
2019-06-10 22:07:26 -0400

    make centos compatable

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+PKG_MANAGER="${PKG_MANAGER}"
+if [ -f /etc/redhat-release ]; then
+    PKG_MANAGER="yum"
+fi
+
 # install i3
-sudo apt update
-sudo apt install -y i3
+sudo ${PKG_MANAGER} update
+sudo ${PKG_MANAGER} install -y i3
 
 #install i3blocks
-sudo apt install -y i3blocks
+sudo ${PKG_MANAGER} install -y i3blocks
 
 
 # install deps
-sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake
+sudo ${PKG_MANAGER} install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake
 
 # install xcb
 cd /tmp
@@ -36,16 +41,16 @@ sudo make install
 
 
 # install rofi
-sudo apt install rofi
+sudo ${PKG_MANAGER} install rofi
 
 # install compton
-sudo apt install compton
+sudo ${PKG_MANAGER} install compton
 
 # install feh
-sudo apt install feh
+sudo ${PKG_MANAGER} install feh
 
 # install deps for termite
-sudo apt-get install -y git g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac intltool libpcre2-dev libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev libxml2-utils gperf
+sudo ${PKG_MANAGER} install -y git g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac intltool libpcre2-dev libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev libxml2-utils gperf
 
 
 # install vte lib
@@ -68,18 +73,18 @@ sudo ln -s /usr/local/share/terminfo/x/xterm-termite /lib/terminfo/x/xterm-termi
 sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite 60
 
 # install conky
-sudo apt install conky
+sudo ${PKG_MANAGER} install conky
 
 # install font-awesome
-sudo apt install fonts-font-awesome
+sudo ${PKG_MANAGER} install fonts-font-awesome
 
 # install py3status
-sudo apt install py3status
+sudo ${PKG_MANAGER} install py3status
 
 # Optional: install neofetch
-sudo apt install neofetch
+sudo ${PKG_MANAGER} install neofetch
 
 # Optional: install cmatrix
-sudo apt install cmatrix
+sudo ${PKG_MANAGER} install cmatrix
 
 
